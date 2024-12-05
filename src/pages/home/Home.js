d9205878e18316f1c7f3682538e7fa03dc8b4507
Renato R Ferreira <renatorf0910@gmail.com>
2024-12-05 20:26:02 -0300

    .

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,6 +1,20 @@
+import axios from 'axios';
 import React from 'react';
+import { useQuery } from 'react-query';
 
 const Home = () => {
+    const { data, isLoading } = useQuery('users', () => {
+        return axios
+            .get("http://localhost:8000/api/user")
+            .then((response) => response.data);
+    });
+
+    console.log("data:", data)
+
+    if (isLoading) {
+        return <div className='loading'>Loading...</div>
+    }
+
     return (
         <div>
             Home
